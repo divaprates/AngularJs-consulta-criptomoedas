93c222138630a36c24aae9efba41168142e53212
divaprates <divasilvaprates@gmail.com>
2023-11-01 23:23:32 -0300

    feat: adicionando um botão loadMore

--- app/modules/controllers/dashboard.controller.js
+++ app/modules/controllers/dashboard.controller.js
@@ -3,14 +3,30 @@ app.controller('DashboardController', DashboardController);
 function DashboardController($scope, apiService) {
     var vm = $scope;
     vm.oi = 'olá';
+    vm.coins = [];
+    vm.pagina = 1;
+    vm.numMoedas = 20;
 
-    apiService
-        .getData()
-        .then(result => {
-            
-            if(result.data) {
-                vm.coins = result.data;
-            }
-            
-        });
+    vm.init = () => {
+        vm.carregarDados();
+    }
+    
+    vm.carregarDados = () => {
+        apiService
+            .getData(vm.numMoedas, vm.pagina)
+            .then(result => {
+                
+                if(result.data) {
+                    vm.coins = [...vm.coins, ...result.data];
+                    vm.pagina++;
+                }
+                
+            });
+    }
+
+    vm.loadMore = () => {
+        vm.carregarDados();
+    }
+
+    vm.init();
 }
--- app/modules/services/apiRequest.service.js
+++ app/modules/services/apiRequest.service.js
@@ -2,10 +2,14 @@ app.factory('apiService', ['$http', function ($http) {
     var URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en';
 
     return {
-        getData: function() {
+        getData: function(per_page, page) {
             return $http({
                 method: 'GET',
-                url: URL
+                url: URL,
+                params: {
+                    per_page,
+                    page
+                }
             })
         }
     }
